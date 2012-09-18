19fbb0e62a08e68d63d68f980537f7f0847f91bc
Veselin Todorov <veselin@veselin.bg>
2012-09-18 17:57:20 +0300

    Add some examples

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # TODO
 
-## Future TODO
-
+- sane readme
+- docs
 - test helpers
 - schema
 - validation
 - associations
 - handle errors
 - chai test helpers
+- handle xml
+- include root element or not
+- Resource.member()
---
+++ examples/basic_auth.js
@@ -0,0 +1,21 @@
+var Resource = require('..').Resource;
+var BasicAuth = require('..').BasicAuth;
+
+
+var Base = Resource.extend({
+  host: 'http://localhost:3000/api/v1',
+  ext: 'json',
+  auth: { type: BasicAuth, user: '5ea983e97362a95c20b0491cb78cc633', pass: '2e0f80f0472dd73843fd882a1944e62d' }
+});
+
+var Project = Base.extend({
+  path: '/projects'
+});
+
+var Error = Base.extend({
+  path: '/projects/:project_id/errors'
+});
+
+Error.all({ project_id: 1 }, function(err, projects) {
+  console.log(projects[0]);
+});
